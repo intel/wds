289a40b81509d95b4ae368ef5db6919d255b8a21
Alexander Kanavin <alex.kanavin@gmail.com>
2015-04-10 14:14:10 +0300

    Adjust the copyright year

--- mirac_network/mirac-gst-bus-handler.cpp
+++ mirac_network/mirac-gst-bus-handler.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of Wireless Display Software for Linux OS
  *
- * Copyright (C) 2014 Intel Corporation.
+ * Copyright (C) 2015 Intel Corporation.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
